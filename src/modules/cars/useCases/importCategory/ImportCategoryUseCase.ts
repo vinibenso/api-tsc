d8220708fc9fcd4462256b05db3ac018ce868dc5
Vinicius Benso <vinibenso@gmail.com>
2022-08-23 16:09:47 -0300

    docker compose error

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -43,7 +43,7 @@ class ImportCategoryUseCase {
 
     categories.map(async (category) => {
       const {name, description} = category;
-      const existCategory = this.categoryRepository.findByName(name);
+      const existCategory = await this.categoryRepository.findByName(name);
 
       if(!existCategory){
         this.categoryRepository.create({
