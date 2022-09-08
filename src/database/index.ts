3b8abe8d0edb7332ead45edbe05737915222d295
Vinicius Benso <vinibenso@gmail.com>
2022-09-08 12:19:19 -0300

    docker postgress

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,21 +1,3 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { User } from "../modules/accounts/entities/User";
-import { Category } from 
-import { Specification } from "../modules/cars/entities/Specification";
-//docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
-//yarn typeorm migration:create src/database/migrations/CreateCategories
+import { createConnection } from "typeorm";
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "database",
-    port: 5432,
-    username: "docker",
-    password: "1234",
-    database: "rentx",
-    entities: [Category, Specification, User],
-    migrations: ["src/database/migrations/*.ts"],
-})
-
-AppDataSource.initialize().then(async () => {
-    console.log("Initializing the database...")
+createConnection()
